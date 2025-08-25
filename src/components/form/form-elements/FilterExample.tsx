672c5dd1f33d973f1a4764ebebc2ced0eca78868
ridwankimin <mochamadridwan008@gmail.com>
2025-08-25 10:52:01 +0700

    nambah filter upt

--- src/components/form/form-elements/FilterExample.tsx
+++ src/components/form/form-elements/FilterExample.tsx
@@ -19,9 +19,34 @@ export default function FilterExample({ onDataFiltered }: FilterExampleProps) {
   const username = "imigrasiok";
   const password = "6SyfPqjD68RRQKe";
   const uptList = [
-    { id: "5100", nama: "BBKHIT Bali" },
-    { id: "3500", nama: "BKHIT Jawa Timur" },
-    { id: "3600", nama: "BKHIT Banten" },
+    { id:"1100", nama: "BKHIT Aceh"},
+    { id:"1200", nama: "BBKHIT Sumatera Utara"},
+    { id:"1300", nama: "BKHIT Sumatera Barat"},
+    { id:"1400", nama: "BKHIT Riau"},
+    { id:"1600", nama: "BKHIT Sumatera Selatan"},
+    { id:"1800", nama: "BKHIT Lampung"},
+    { id:"1900", nama: "BKHIT Kepulauan Bangka Belitung"},
+    { id:"2100", nama: "BKHIT Kepulauan Riau"},
+    { id:"3100", nama: "BBKHIT DKI Jakarta"},
+    { id:"3200", nama: "BKHIT Jawa Barat"},
+    { id:"3300", nama: "BKHIT Jawa Tengah"},
+    { id:"3400", nama: "BKHIT Daerah Istimewa Yogyakarta"},
+    { id:"3500", nama: "BKHIT Jawa Timur"},
+    { id:"3600", nama: "BKHIT Banten"},
+    { id:"5100", nama: "BBKHIT Bali"},
+    { id:"5300", nama: "BKHIT Nusa Tenggara Timur"},
+    { id:"6100", nama: "BKHIT Kalimantan Barat"},
+    { id:"6300", nama: "BKHIT Kalimantan Selatan"},
+    { id:"6400", nama: "BBKHIT Kalimantan Timur"},
+    { id:"6500", nama: "BKHIT Kalimantan Utara"},
+    { id:"7100", nama: "BKHIT Sulawesi Utara"},
+    { id:"7200", nama: "BKHIT Sulawesi Tengah"},
+    { id:"7300", nama: "BBKHIT Sulawesi Selatan"},
+    { id:"8100", nama: "BKHIT Maluku"},
+    { id:"9100", nama: "BBKHIT Papua"},
+    { id:"9300", nama: "BKHIT Papua Selatan"},
+    { id:"9600", nama: "BKHIT Papua Barat Daya" },
+    
   ];
   const fetchData = async () => {
     try {
